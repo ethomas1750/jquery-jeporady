fbcf1e9d1f997ae544e539647550d61785b29f6c
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-09 20:52:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 console.log(function)
 
+$(document).ready(function() {
+    $('.question').click(function() {
+        var questionText = $(this).data('question');
+        var answerText = $(this).data('answer');
+        
+        $('#question-text').text(questionText);
+        $('#answer-text').text(answerText).hide();
+        $('#question-modal').show();
+    });
+
+    $('.close').click(function() {
+        $('#question-modal').hide();
+    });
+
+    $('#show-answer').click(function() {
+        $('#answer-text').show();
+    });
+});
